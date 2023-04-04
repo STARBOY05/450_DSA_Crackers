9f8cd503a05017b0a5fd69f2eb314cef5fb9404b
Alistair Saldanha <shaanalistair47@gmail.com>
2023-04-04 09:23:06 +0530

    Added another technique of getting subArrays

--- Arrays/Q_22_Generate_SubArrays.cpp
+++ Arrays/Q_22_Generate_SubArrays.cpp
@@ -2,26 +2,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printSubArrays(vector<int> arr, int start, int end)
+// Recursion - Method1
+void printSubArrays1(vector<int> arr, int start, int end)
 {
     if (end == arr.size())
         return;
     else if (start > end)
-        printSubArrays(arr, 0, end + 1);
+        printSubArrays1(arr, 0, end + 1);
     else
     {
         cout << "[";
         for (int i = start; i < end; i++)
             cout << arr[i] << ", ";
         cout << arr[end] << "]" << endl;
-        printSubArrays(arr, start + 1, end);
+        printSubArrays1(arr, start + 1, end);
     }
     return;
 }
 
+// Recursion - Method2
+void printSubArrays2(vector<int> &arr, vector<int> subArr, int idx)
+{
+    // Base Case
+    if (idx == arr.size())
+    {
+        for (auto i : subArr)
+            cout << i << " ";
+        cout << endl;
+        return;
+    }
+    // Take the element
+    subArr.push_back(arr[idx]);
+    printSubArrays2(arr, subArr, idx + 1);
+    // Don't take the element
+    subArr.pop_back();
+    printSubArrays2(arr, subArr, idx + 1);
+}
+
 int main()
 {
-    vector<int> arr = {1, 2, 3};
-    printSubArrays(arr, 0, 0);
+    vector<int> arr = {3, 1, 2};
+    printSubArrays1(arr, 0, 0);
+    vector<int> subArr;
+    printSubArrays2(arr, subArr, 0);
     return 0;
 }
